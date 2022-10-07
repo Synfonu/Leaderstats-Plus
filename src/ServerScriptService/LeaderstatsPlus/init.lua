4e0fe083d65a0942d5b2b2ddf19ae675879355ee
Noxirity <94649429+Noxirity@users.noreply.github.com>
2022-10-07 20:50:27 +0100

    Rename

--- src/ServerScriptService/LeaderstatsPlus/init.lua
+++ src/ServerScriptService/LeaderstatsPlus/init.lua
@@ -1,9 +1,9 @@
 --[[
-    ❓ EasyStat is a simple service designed to make Data saving and leaderstats easier
+    ❓ Leaderstats Plus is a simple service designed to make Data saving and leaderstats easier
     than ever to use. With built-in data saving and leaderstats, you don't need to worry
     about making your own systems to manage this stuff.
 
-    📁 EasyStat is registered with MIT license, you can find more information on LICENSE
+    📁 Leaderstats Plus is registered with MIT license, you can find more information on LICENSE
     file in the GitHub repository: https://github.com/Synfonu/EasyStat
 
 --]]
